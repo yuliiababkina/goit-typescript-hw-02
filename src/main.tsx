f453f62f1e7426e90cf2e026306f9802d1fb3973
yuliia.babkina <yulichka.babkina1312@gmail.com>
2025-02-10 20:08:38 -0300

    updated goit-typescript-hw-02

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./components/App/App.module.css";
+import { Toaster } from "react-hot-toast";
 
 import App from "./components/App/App";
-import { Toaster } from "react-hot-toast";
+import "./components/App/App.module.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
